751d2c185cfdf23b96ad8ba67a18769c8b6616df
Valeria Copleman <valeriacopleman@gmail.com>
2021-02-24 15:33:10 -0800

    commit tree 687f8da55686da9bf5d7326e95ed9fe751dc9dcc

--- index.js
+++ index.js
@@ -10,4 +10,24 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(string) {
+  string = string.split(":")
+  let time = string.join("")
+  if (time < 1200) {
+    return "Good Morning";
+  }
+  else if ((time >= 1200) && (time <= 1700)) {
+    return "Good Afternoon";
+  }
+  else if (time > 1700) {
+    return "Good Evening";
+  }
+
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(TEST) {
+  document.getElementById('greeting').innerText = TEST;
+}
